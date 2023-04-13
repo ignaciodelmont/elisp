c6c2cd33654922c68c9a0fdbf79da4870d2a769d
Ignacio Delmont <ignaciodelmont@gmail.com>
2023-04-12 22:11:14 -0300

    add autovenv

--- python-configs.el
+++ python-configs.el
@@ -2,14 +2,22 @@
   :ensure t)
 
 
+(use-package auto-virtualenv
+  :ensure t
+  :init
+  (use-package pyvenv
+    :ensure t)
+  :config
+  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv)  ;; If using projectile
+  )
+
+
 (use-package lsp-pyright
   :ensure t
-  :hook (python-mode . (lambda ()
-			 
-			 (with-eval-after-load 'poetry
-			   (poetry-venv-workon)) ; ensure poetry venv is activated before lsp is triggered
-			 
-			 (require 'lsp-pyright)			
+  :hook (python-mode . (lambda ()	
+			 (require 'auto-virtualenv)
+			 (require 'lsp-pyright)
+			 (auto-virtualenv-set-virtualenv)
 			 (lsp)
 			 (flycheck-mode 1)
 			 (flycheck-select-checker 'python-flake8))))
